b661d9adfc12363f33cce21fc4eade44dd0776c2
Hugo Gavilima UDLA <hugo.gavilima@udla.edu.ec>
2023-11-05 15:01:19 -0500

    Creamos el paquete

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='komisan',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "optbinning==0.18.0",
+        "imblearn",
+        "scikit-learn==1.3.2",
+        "statsmodels"
+    ],
+)
